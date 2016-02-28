98d22eddaca549ae0b7f49454c8ac1fd802eb887
alex <alex@quist.ca>
2016-02-27 21:06:44 -0500

    Added code to GetChunk()

--- IADa2/IADa2/FileWrapper.cpp
+++ IADa2/IADa2/FileWrapper.cpp
@@ -8,6 +8,14 @@ FileWrapper::FileWrapper(std::string filePath, int chunkSize)
 
 void FileWrapper::GetChunk(char* chunk, int index)
 {
+	ifstream ifs;
+	ifs.open(filePath, std::ifstream::in);
+
+	ifs.seekg(index*chunkSize, ifs.beg);
+
+	ifs.read(chunk, chunkSize);
+
+	ifs.close();
 }
 
 void FileWrapper::WriteChunk(char * chunk, int index)
